3c72551c141bf916c2d708cee3fcbd5046c35a06
810eigo-droid <810eigo@gmail.com>
2026-09-15 13:52:00 +0900

    Remove counseling LP after verified migration to 810eigo-droid/korekara

--- korekara-counseling/dist/ambient.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const preference = window.matchMedia('(prefers-reduced-motion: reduce)');
-  const render = () => {
-    document.documentElement.dataset.motion = preference.matches ? 'paused' : 'running';
-  };
-  preference.addEventListener('change', render);
-  render();
-})();
